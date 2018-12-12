4d581c9f31daf28d7d5a28573bb3697fe8144ca0
ivica petrsoric <ivica.petrsoric@codecacao.com>
2018-12-12 22:30:15 +0100

    Building UI with Stack Views
    Signed-off-by: ivica petrsoric <ivica.petrsoric@codecacao.com>

--- LikeTinder/ViewController.swift
+++ LikeTinder/ViewController.swift
@@ -14,6 +14,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        let subviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { (color) -> UIView in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subviews)
+        topStackView.distribution = .fillEqually
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
+//        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        
+        view.addSubview(stackView)
+        stackView.fillSuperview()
     }
 
 
