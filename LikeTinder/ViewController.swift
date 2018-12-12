fd67212dee9960e5e917a816343d8f9c4fc45289
ivica petrsoric <ivica.petrsoric@codecacao.com>
2018-12-12 21:54:32 +0100

    Initial Commit

---
+++ LikeTinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LikeTinder
+//
+//  Created by ivica petrsoric on 12/12/2018.
+//  Copyright © 2018 ivica petrsoric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
